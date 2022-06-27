8a6b14eab5f6d784d0d7a467efa484cc004b50ca
JesseNexcau <agencyjessenexcau@gmail.com>
2022-06-27 08:51:30 -0300

    Projeto começou a dar certo dessa forma

--- src/pages/home/Home.js
+++ src/pages/home/Home.js
@@ -7,13 +7,62 @@ import './Home.css';
 
 
 function CreateCertificate(){
-
       
+      var [layer, setNewLayer] = useState(
+            {
+                  sizeX: 0, 
+                  sizeY: 0, 
+                  color: '',
+                  image: '',
+                  title: '', 
+                  name: '', 
+                  msg1: '', 
+                  msg2: '', 
+                  msg3: '', 
+                  footer: ''
+            });
+
+      // useEffect(()=>{
+
+      // })
+
+      const setCertificateSize = (sizeX, sizeY) =>{
+            let certSize = {sizeX: sizeX, sizeY: sizeY};
+            
+            setNewLayer(layer => ({
+                  ...layer,
+                  ...certSize
+            }));
+      }
+      const setCertificateBackground = (mode, image, color) =>{
+            if(mode==='color'){
+                  let certColor = {color: color};
+
+                  setNewLayer(layer = ({
+                        ...layer,
+                        ...certColor
+                  }));
+            }else{
+
+            }
+      }
 
       return(
             <>
 
-                  
+                  <div style={{width: `${layer.sizeX}px`, height: `${layer.sizeY}px`, backgroundColor: `${layer.color}`}} className='layer'>
+
+                  </div>
+
+                  <Button onClick={(e)=>{
+                        e.preventDefault();
+                        setCertificateSize(1000,625)
+                  }} txt='Criar Certificado' color='white' bg='rgb(127,0,255)' />
+
+                  <Button onClick={(e)=>{
+                        e.preventDefault();
+                        setCertificateBackground('color','','rgb(255,150,000)');
+                  }} txt='Alterar Cor' color='white' bg='rgb(127,0,255)' />
             </>
       );
       
